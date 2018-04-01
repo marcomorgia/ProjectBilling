de1784932cfdf6ef2d1143d91023fd27a03f903a
Marco Morgia <marco.morgia83@gmail.com>
2018-04-01 10:21:04 +0200

    PrimoCommit

---
+++ ProjectBilling.DataService/DataService.cs
@@ -0,0 +1,37 @@
+﻿using ProjectBilling.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBilling.DataService
+{
+    public interface IDataService
+    {
+        IList<Project> GetProjects();
+    }
+    public class DataService : IDataService
+    {
+        public IList<Project> GetProjects()
+        {
+            List<Project> projects = new List<Project>()
+            {
+                new Project()
+                {
+                    ID = 1,
+                    Name = "Halloway",
+                    Estimate = 500
+                },
+                new Project()
+                {
+                    ID = 2,
+                    Name = "Jones",
+                    Estimate = 1500
+                }
+            };
+
+            return projects;
+        }
+    }
+}
